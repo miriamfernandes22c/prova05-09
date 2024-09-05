fb0f86c6a3a11d415a2405fa1a4b51be8ee7e71d
miriamfernandes22c <165180767+miriamfernandes22c@users.noreply.github.com>
2024-09-05 18:25:45 -0300

    Add files via upload

---
+++ prova.sql
@@ -0,0 +1,19 @@
+CREATE TABLE fornecedores (
+
+INSERT INTO  Nome_Fornecedor:AllTech, Valor_Total: 15.0000
+INSERT INTO  Nome_Fornecedor:TechTudo, Valor_Total: 185.0000
+INSERT INTO  Nome_Fornecedor:LogTech, Valor_Total: 257.0000
+INSERT INTO  Nome_Fornecedor:GlobalTech, Valor_Total: 222.0000
+INSERT INTO  Nome_Fornecedor:SpTech, Valor_Total: 111.0000
+);
+
+CREATE TABLE compras (
+
+INSERT INTO compras_fevereiro, 
+INSERT INTO nome_fornecedores,
+INSERT INTO data_compra,
+VALUES(Nome_Fornecedor:AllTech, Data_Compra:2024-02-09,  Nome_Fornecedor:TechTudo, Data_Compra:2024-02-10, Nome_Fornecedor:LogTech,Data_Compra:2024-02-15, Nome_Fornecedor:GlobalTech,Data_Compra:2024-02-12,Nome_Fornecedor:SpTech,Data_Compra:2024-02-04 )
+);
+
+
+
